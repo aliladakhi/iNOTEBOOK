35dc3a98bf8061a909702f3e9856cc6fb2074a5b
ali <2021ucs0103@gmail.com>
2023-10-16 00:37:16 +0530

    going for signup page

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -37,34 +37,17 @@ router.post(
       //create user
       user = await User.create({
         name: req.body.name,
+
         email: req.body.email,
         password: secPass,
       });
-      //   .then((user) => {
-      //     res.json(user);
-      //   })
-      //   .catch((err) => {
-      //     console.log(err);
-      //     res.json({ error: "please not make redendetn e-mail" });
-      //   });
-
-      // // Save the user to the database
-      // user.save()
-      //     .then(savedUser => {
-      //         console.log('User saved:', savedUser);
-      //         res.status(201).json(savedUser);
-      //     })
-      //     .catch(error => {
-      //         console.error('Error saving user:', error);
-      //         res.status(500).json({ error: 'Internal server error' });
-      //     });
       const data = {
         user: {
           id: user.id,
         },
       };
-      const authToken = jwt.sign(data, JMT_SECT);
-
+      const authToken = jwt.sign(data,JMT_SECT);
+      res.setHeader('Content-Type', 'application/json');
       console.log(authToken);
       res.json({ authToken: authToken });
     } catch (error) {
@@ -73,47 +56,50 @@ router.post(
   }
 );
 
-// Authentication a user using :POST "/api/auth/login". NO login required
-//Route 2
-router.post(
-  "/login",
-  [
-    body("email").isEmail(),
-    body("password", "password should not be blank").exists(),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+// ROUTE 2: Authenticate a User using: POST "/api/auth/login". No login required
+router.post('/login', [
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+  let success = false;
+  // If there are errors, return Bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      success = false
+      return res.status(400).json({ error: "Please try to login with correct credentials" });
     }
 
-    const { email, password } = req.body; //destructuring req.body
-    try {
-      let user = await User.findOne({ email });
-      if (!user) {
-        return res
-          .status(400)
-          .json({ error: "Please try with correct credentials" });
-      }
-      const passwordCompare = await bcrypt.compare(password, user.password);
-      if (!passwordCompare) {
-        return res
-          .status(400)
-          .json({ error: "Please try with correct credentials" });
-      }
-      const data = {
-        user: {
-          id: user.id,
-        },
-      };
-      const authToken = jwt.sign(data, JMT_SECT);
-      res.json({ authToken: authToken });
-    } catch (error) {
-      console.error("Error:", error);
-      res.status(500).send("server Error occured");
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      success = false
+      return res.status(400).json({ success, error: "Please try to login with correct credentials" });
     }
+
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+    const authToken = jwt.sign(data, JMT_SECT);
+    success = true;
+    res.json({ success, authToken })
+    res.setHeader('Content-Type', 'application/json');
+
+  } catch (error) {
+    console.error(error.message);
+    console.log("koko");
+    res.status(500).send("Internal Server Error");
   }
-);
+
+
+});
 
 //Route 3:Get loggin User Details using :POST "/api/auth/getuser".Login required
 router.post("/getuser", fetchuser, async (req, res) => {
--- FrontEnd/src/Components/Login.jsx
+++ FrontEnd/src/Components/Login.jsx
@@ -1,77 +1,58 @@
-import React, { useState } from "react";
-
-function Login() {
-    const [cred,setCred]=useState({email:"",password:""})
-const submitHandle= async (e)=>{
-    try{
-    const response=await fetch("http://localhost:5000/api/auth/login",{
-        method:'POST',
-        header:{
-            'content-Type':'application/json'
-        },
-        body: JSON.stringify({
-            email: cred.email,
-            password: cred.password
-          })
-    })
+import React, {useState} from 'react'
+// import { useHistory } from 'react-router-dom'
 
 
-    if (response.ok) {
-        const json = await response.json();
+const Login = () => {
+    const [credentials, setCredentials] = useState({email: "", password: ""}) 
+    // let history = useHistory();
+    console.log(3)
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      console.log(3)
+        const response = await fetch("http://localhost:5000/api/auth/login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email: credentials.email, password: credentials.password})
+        });
+        const json = await response.json()
+        // const responseText = await response.text();
+// console.log('Response Text:', responseText);
+
         console.log(json);
-      } else {
-        console.error("Error in fetch request:", response.status);
-      }
-    }catch (error) {
-      console.error("An error occurred:", error);
+        if (json.success){
+            // Save the auth token and redirect
+            localStorage.setItem('token', json.authToken); 
+            // history.push("/");
+
+        }
+        else{
+            alert("Invalid credentials");
+        }
     }
 
+    const onChange = (e)=>{
+        setCredentials({...credentials, [e.target.name]: e.target.value})
+    }
 
-}
-const changeHandlePass=(e)=>{
-    setCred({...cred,password: e.target.value})
-}
-const changeHandle=(e)=>{
-    setCred({...cred,email: e.target.value})
-}
-  return (
-    <div>
-      <form >
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
-          <input
-          onChange={changeHandle}
-          value={cred.email}
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
-          </div>
+    return (
+        <div>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" value={credentials.email} onChange={onChange} id="email" name="email" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" value={credentials.password} onChange={onChange} name="password" id="password" />
+                </div>
+
+                <button  className="btn btn-primary" onClick={handleSubmit}>Submit</button>
+            </form>
         </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
-            Password
-          </label>
-          <input
-          value={cred.password}
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            onChange={changeHandlePass}
-          />
-        </div>
-
-        <button type="submit" className="btn btn-primary" onClick={submitHandle}>
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+    )
 }
 
-export default Login;
+export default Login
